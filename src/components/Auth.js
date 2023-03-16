673cb00cda6c0edc08999df6f7c564a6cfe85de4
Sharonjep <jepngetichs65@gmail.com>
2023-03-16 14:40:47 +0300

    login page

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,31 +1,81 @@
-import React from "react"
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [resetUsername, setResetUsername] = useState('');
 
-export default function (props){
-    return (
-        <div className ="Auth-form-container">
-            <form className="Auth-form">
-                <div className="Auth-form-content">
-                    <h3 className ="Auth-form-title">Sign In</h3>
-                    <div className ="form-group mt-3">
-                        <label>Email address</label>
-                        <input type="email" className="form-control mt-1" placeholder="Enter email"/>
+  const handleLogin = () => {
+    // Send login credentials to server for validation
+    fetch('/login', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.success) {
+        alert('Login successful!');
+      } else {
+        alert('Invalid login credentials. Please try again.');
+      }
+    });
+  };
 
-                    </div>
-                    <div className ="form-group mt-3">
-                        <label>Password</label>
-                        <input type="password"className="form-control mt-1" placeholder="Enter password"/>
+  const handleResetPassword = () => {
+    // Send username to server for password reset
+    fetch('/reset-password', {
+      method: 'POST',
+      body: JSON.stringify({ username: resetUsername }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.success) {
+        alert('Password reset successful! Please check your email for instructions.');
+      } else {
+        alert('Unable to reset password. Please check your username and try again.');
+      }
+    });
+  };
 
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-body">
+              <h3 className="card-title">Login</h3>
+              <form>
+                <div className="form-group">
+                  <label>Username</label>
+                  <input type="text" className="form-control" value={username} onChange={e => setUsername(e.target.value)} />
                 </div>
-                <div className ="d-grid gap-2 mt-3">
-                    <button type ="submit" className="btn btn-primary">Submit</button>
-
+                <div className="form-group">
+                  <label>Password</label>
+                  <input type="password" className="form-control" value={password} onChange={e => setPassword(e.target.value)} />
                 </div>
-                <p className ="forgot-password text-right mt-2">
-                    Forgot <a href ="#">password</a>
-                </p>
+                <button type="button" className="btn btn-primary" onClick={handleLogin}>Login</button>
+              </form>
+            </div>
+          </div>
+          <div className="card mt-3">
+            <div className="card-body">
+              <h4 className="card-title">Reset Password</h4>
+              <form>
+                <div className="form-group">
+                  <label>Username</label>
+                  <input type="text" className="form-control" value={resetUsername} onChange={e => setResetUsername(e.target.value)} />
                 </div>
-            </form>
+                <button type="button" className="btn btn-primary" onClick={handleResetPassword}>Reset Password</button>
+              </form>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
+
+export default App;
